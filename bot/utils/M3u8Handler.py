949ae7cbe64ca117705a631d9ae54eab933187a9
neilabbiebebe <78553288+neilabbiebebe@users.noreply.github.com>
2024-05-18 11:07:14 +0800

    Update M3u8Handler.py

--- bot/utils/M3u8Handler.py
+++ bot/utils/M3u8Handler.py
@@ -1,4 +1,6 @@
-import asyncio, aiohttp, m3u8
+import asyncio
+import aiohttp
+import m3u8
 from pyrogram.types import Message
 from config import WEBSITE_DOMAIN
 from utils.Database import save_file
@@ -9,19 +11,13 @@ from typing import Literal
 
 logger = Logger(__name__)
 
-
-async def Single_M3U8_Uploader(
-    session: aiohttp.ClientSession, proc: Message, m3u8: str, hash: str
-):
+async def Single_M3U8_Uploader(session: aiohttp.ClientSession, proc: Message, m3u8: str, hash: str):
     await asyncio.sleep(5)
-
     try:
-        await proc.edit(
-            "📤 **Uploading File To Telegram...**\n\n⚠️ **Note :** If the file is too large, it may take some time to upload all files. Please be patient."
-        )
+        await proc.edit("📤 **Uploading File To Telegram...**\n\n⚠️ **Note :** If the file is too large, it may take some time to upload all files. Please be patient.")
     except Exception as e:
         logger.warning(e)
-
+    
     logger.info(f"Uploading File {hash}")
     await asyncio.sleep(1)
 
@@ -30,16 +26,7 @@ async def Single_M3U8_Uploader(
     with open(m3u8, "r") as f:
         m3u8_file_text = f.read()
 
-    tsFiles = []
-
-    for line in m3u8_file_text.splitlines():
-        line = line.strip()
-        if line.endswith(".ts"):
-            line = line.strip()
-            tsFiles.append(line)
-
-    for i in range(len(tsFiles)):
-        tsFiles[i] = dir + "/" + tsFiles[i]
+    tsFiles = [dir + "/" + line.strip() for line in m3u8_file_text.splitlines() if line.strip().endswith(".ts")]
 
     tsData, new_file_list = await Multi_TS_File_Uploader(session, tsFiles, proc, hash)
 
@@ -57,12 +44,9 @@ async def Single_M3U8_Uploader(
         except FloodWait as e:
             logger.error(f"FloodWait Error : {e}")
             try:
-                await proc.edit(
-                    f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds"
-                )
+                await proc.edit(f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds")
             except Exception as e:
                 logger.warning(e)
-
             await asyncio.sleep(e.value)
             continue
         except Exception as e:
@@ -75,23 +59,12 @@ async def Single_M3U8_Uploader(
         logger.warning(e)
 
     logger.info(f"Uploaded Successfully {hash}")
-
     await save_file(hash, tsData)
     logger.info(f"Saved to Database {hash}")
     await asyncio.sleep(5)
     return f"index_c{channel}.m3u8"
 
-
-async def Multi_M3U8_Uploader(
-    session: aiohttp.ClientSession,
-    proc: Message,
-    m3u8_url: str,
-    headers: dict,
-    hash: str,
-    name: str,
-    master_m3u8_text: str,
-    _type: Literal["video", "audio"],
-):
+async def Multi_M3U8_Uploader(session: aiohttp.ClientSession, proc: Message, m3u8_url: str, headers: dict, hash: str, name: str, master_m3u8_text: str, _type: Literal["video", "audio"]):
     await asyncio.sleep(5)
     async with session.get(m3u8_url, headers=headers) as r:
         m3u8_file_text = await r.text()
@@ -104,20 +77,15 @@ async def Multi_M3U8_Uploader(
         pos += 1
         file_url = segment.uri
         ts_name = f"{hash}_part{pos}.ts"
-
         file_list.append((ts_name, file_url))
 
     try:
-        tsData, file_list = await Multi_TS_DL_And_Uploader(
-            session, file_list, proc, hash, name, headers
-        )
+        tsData, file_list = await Multi_TS_DL_And_Uploader(session, file_list, proc, hash, name, headers)
     except Exception as e:
-        # Handle if one quality fails
         try:
             await proc.edit(f"❌ **Failed To Upload {name} :** {e}")
         except Exception as e:
             logger.warning(e)
-
         await asyncio.sleep(5)
 
         item_to_be_removed = m3u8_url.split("/")[-1]
@@ -130,7 +98,7 @@ async def Multi_M3U8_Uploader(
                 item_pos = pos
                 break
 
-        master_m3u8_text = "\n".join(lines[: item_pos - 1] + lines[item_pos + 1 :])
+        master_m3u8_text = "\n".join(lines[:item_pos-1] + lines[item_pos+1:])
         return {}, master_m3u8_text
 
     for name, url in file_list:
@@ -146,23 +114,16 @@ async def Multi_M3U8_Uploader(
             msg, channel = await send_file(session, m3u8_file_text.encode(), True)
             tsData[f"{hash}_c{channel}.m3u8"] = msg["message_id"]
             if _type == "video":
-                master_m3u8_text = master_m3u8_text.replace(
-                    m3u8_url.split("/")[-1], f"{hash}_c{channel}.m3u8"
-                )
+                master_m3u8_text = master_m3u8_text.replace(m3u8_url.split("/")[-1], f"{hash}_c{channel}.m3u8")
             if _type == "audio":
-                master_m3u8_text = master_m3u8_text.replace(
-                    "/".join(m3u8_url.split("/")[-3:]), f"{hash}_c{channel}.m3u8"
-                )
+                master_m3u8_text = master_m3u8_text.replace("/".join(m3u8_url.split("/")[-3:]), f"{hash}_c{channel}.m3u8")
             break
         except FloodWait as e:
             logger.error(f"FloodWait Error : {e}")
             try:
-                await proc.edit(
-                    f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds"
-                )
+                await proc.edit(f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds")
             except Exception as e:
                 logger.warning(e)
-
             await asyncio.sleep(e.value)
             continue
         except Exception as e:
@@ -172,15 +133,7 @@ async def Multi_M3U8_Uploader(
     await asyncio.sleep(5)
     return tsData, master_m3u8_text
 
-
-async def Video_Handler(
-    session: aiohttp.ClientSession,
-    proc: Message,
-    data: dict,
-    headers: dict,
-    hash: str,
-    master_m3u8_text,
-):
+async def Video_Handler(session: aiohttp.ClientSession, proc: Message, data: dict, headers: dict, hash: str, master_m3u8_text):
     combined_video_data = {}
     await asyncio.sleep(1)
 
@@ -192,110 +145,52 @@ async def Video_Handler(
             logger.warning(e)
         await asyncio.sleep(1)
 
-        tsData, master_m3u8_text = await Multi_M3U8_Uploader(
-            session,
-            proc,
-            url,
-            headers,
-            f"{hash}_{q}p",
-            f"{q}p Video",
-            master_m3u8_text,
-            "video",
-        )
+        tsData, master_m3u8_text = await Multi_M3U8_Uploader(session, proc, url, headers, f"{hash}_{q}p", f"{q}p Video", master_m3u8_text, "video")
         combined_video_data.update(tsData)
 
-        logger.info(f"Uploaded {q}p Quality Video To Telegram...")
-        try:
-            await proc.edit(f"📤 **Uploaded {q}p Quality Video To Telegram...**")
-        except Exception as e:
-            logger.warning(e)
-        await asyncio.sleep(1)
+        logger.info(f"Uploaded {q}p Quality Video {hash}")
 
     return combined_video_data, master_m3u8_text
 
-
-async def Audio_Handler(
-    session: aiohttp.ClientSession,
-    proc: Message,
-    data: dict,
-    headers: dict,
-    hash: str,
-    master_m3u8_text: str,
-):
+async def Audio_Handler(session: aiohttp.ClientSession, proc: Message, data: dict, headers: dict, hash: str, master_m3u8_text):
     combined_audio_data = {}
     await asyncio.sleep(1)
-    pos = 0
 
     for t, url in data["audio_url"]:
-        pos += 1
-
-        logger.info(f"Uploading Audio {pos} ({t}) To Telegram...")
+        logger.info(f"Uploading {t} Audio To Telegram...")
         try:
-            await proc.edit(f"📤 **Uploading Audio {pos} ({t}) To Telegram...**")
+            await proc.edit(f"📤 **Uploading {t} Audio To Telegram...**")
         except Exception as e:
             logger.warning(e)
         await asyncio.sleep(1)
 
-        tsData, master_m3u8_text = await Multi_M3U8_Uploader(
-            session,
-            proc,
-            url,
-            headers,
-            f"{hash}_audio_{pos}",
-            f"Audio {pos} ({t})",
-            master_m3u8_text,
-            "audio",
-        )
+        tsData, master_m3u8_text = await Multi_M3U8_Uploader(session, proc, url, headers, f"{hash}_{t}", f"{t} Audio", master_m3u8_text, "audio")
         combined_audio_data.update(tsData)
 
-        logger.info(f"Uploaded Audio {pos} ({t}) To Telegram...")
-        try:
-            await proc.edit(f"📤 **Uploaded Audio {pos} ({t}) To Telegram...**")
-        except Exception as e:
-            logger.warning(e)
-        await asyncio.sleep(1)
+        logger.info(f"Uploaded {t} Audio {hash}")
 
     return combined_audio_data, master_m3u8_text
 
-
-async def Subtitle_Handler(
-    session: aiohttp.ClientSession,
-    proc: Message,
-    data: dict,
-    headers: dict,
-    hash: str,
-):
-    await asyncio.sleep(5)
-    total_files = len(data["subtitle_url"])
-    if total_files == 0:
-        return {}, "", {}
-
+async def Subtitle_Handler(session: aiohttp.ClientSession, proc: Message, data: dict, headers: dict, hash: str):
     combined_subtitle_data = {}
-    await asyncio.sleep(1)
-    pos = 0
-    subtile_text = "🗒 **Subtitle Files :**\n\n"
     subtitle_data = {}
+    subtile_text = ""
+
+    await asyncio.sleep(1)
 
     for t, url in data["subtitle_url"]:
-        pos += 1
-
-        logger.info(f"Uploading subtitle_{pos} ({t}) To Telegram...")
+        logger.info(f"Uploading {t} Subtitle To Telegram...")
         try:
-            await proc.edit(f"📤 **Uploading Subtitle {pos} ({t}) To Telegram...**")
+            await proc.edit(f"📤 **Uploading {t} Subtitle To Telegram...**")
         except Exception as e:
             logger.warning(e)
         await asyncio.sleep(1)
 
         async with session.get(url, headers=headers) as r:
-            if r.status != 200:
-                raise Exception("Failed To Download Video Segment")
-
             bytes_data = await r.read()
-
             file_size = int(r.headers.get("Content-Length", 0))
             if file_size == 0:
                 file_size = len(bytes_data)
-
             if file_size > (19.5 * 1024 * 1024):
                 raise Exception("Too High Video Bitrate")
 
@@ -305,21 +200,16 @@ async def Subtitle_Handler(
                     raise Exception("Failed to upload ts file...")
                 try:
                     msg, channel = await send_file(session, bytes_data, True)
-                    combined_subtitle_data[f"subtitle_{pos}_c{channel}.srt"] = msg[
-                        "message_id"
-                    ]
+                    combined_subtitle_data[f"subtitle_{pos}_c{channel}.srt"] = msg["message_id"]
                     subtile_text += f"🔹 Subtitle {pos} ({t}) : {WEBSITE_DOMAIN}/file/{hash}/subtitle_{pos}_c{channel}.srt \n"
                     await asyncio.sleep(1)
                     break
                 except FloodWait as e:
                     logger.error(f"FloodWait Error : {e}")
                     try:
-                        await proc.edit(
-                            f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds"
-                        )
+                        await proc.edit(f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds")
                     except Exception as e:
                         logger.warning(e)
-
                     await asyncio.sleep(e.value)
                     continue
                 except Exception as e:
@@ -331,10 +221,7 @@ async def Subtitle_Handler(
     await asyncio.sleep(5)
     return combined_subtitle_data, subtile_text, subtitle_data
 
-
-async def Master_Handler(
-    session: aiohttp.ClientSession, proc: Message, data: dict, headers: dict, hash: str
-):
+async def Master_Handler(session: aiohttp.ClientSession, proc: Message, data: dict, headers: dict, hash: str):
     await asyncio.sleep(5)
     try:
         await proc.edit("🔄 **Extracting Video URLs And Data...**")
@@ -346,21 +233,15 @@ async def Master_Handler(
     master_data = {}
 
     # Upload Video Files
-    combined_video_data, master_m3u8_text = await Video_Handler(
-        session, proc, data, headers, hash, master_m3u8_text
-    )
+    combined_video_data, master_m3u8_text = await Video_Handler(session, proc, data, headers, hash, master_m3u8_text)
     master_data.update(combined_video_data)
 
     # Upload Audio Files
-    combined_audio_data, master_m3u8_text = await Audio_Handler(
-        session, proc, data, headers, hash, master_m3u8_text
-    )
+    combined_audio_data, master_m3u8_text = await Audio_Handler(session, proc, data, headers, hash, master_m3u8_text)
     master_data.update(combined_audio_data)
 
     # Upload Subtitle Files
-    combined_subtitle_data, subtile_text, subtitle_data = await Subtitle_Handler(
-        session, proc, data, headers, hash
-    )
+    combined_subtitle_data, subtile_text, subtitle_data = await Subtitle_Handler(session, proc, data, headers, hash)
     master_data.update(combined_subtitle_data)
 
     # Upload Master M3U8 File
@@ -377,12 +258,9 @@ async def Master_Handler(
         except FloodWait as e:
             logger.error(f"FloodWait Error : {e}")
             try:
-                await proc.edit(
-                    f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds"
-                )
+                await proc.edit(f"⚠️ **FloodWait Error :** Sleeping for {e.value} seconds")
             except Exception as e:
                 logger.warning(e)
-
             await asyncio.sleep(e.value)
             continue
         except Exception as e:
@@ -396,13 +274,11 @@ async def Master_Handler(
         logger.warning(e)
 
     await asyncio.sleep(1)
-
     await save_file(hash, master_data, subtitle_data)
     logger.info(f"Saved to Database {hash}")
     await asyncio.sleep(5)
 
     return subtile_text, m3u8
-
 
 async def Master_Extractor(session: aiohttp.ClientSession, data: dict, headers: dict):
     master_url = data["video_url"]
@@ -410,19 +286,10 @@ async def Master_Extractor(session: aiohttp.ClientSession, data: dict, headers: 
         content = await r.text()
 
     master_m3u8_text = content
-
     m3u8_obj = m3u8.loads(content)
     path = "/".join(master_url.split("/")[:-1])
-    qualities = []
-
-    for quality in m3u8_obj.playlists:
-        qualities.append((quality.stream_info.resolution[1], path + "/" + quality.uri))
-
-    audios = []
-    for i in m3u8_obj.media:
-        lang = (i.language or "Unknown").upper()
-        name = i.name
-        audios.append((f"{lang} - {name}", path + "/" + i.uri))
+    qualities = [(quality.stream_info.resolution[1], path + "/" + quality.uri) for quality in m3u8_obj.playlists]
+    audios = [(f"{(i.language or 'Unknown').upper()} - {i.name}", path + "/" + i.uri) for i in m3u8_obj.media]
 
     data["video_url"] = qualities
     data["audio_url"] = audios
